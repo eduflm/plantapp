3c5f0d682333d8a398f32446b5309247340deb87
limaeduardo <eduardolima.dev@gmail.com>
2018-12-10 23:55:24 -0200

    adiciona o micker de medida

--- src/Screens/NovoHistorico.js
+++ src/Screens/NovoHistorico.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, Alert,KeyboardAvoidingView } from 'react-native'
+import { Text, View, StyleSheet, Alert,KeyboardAvoidingView, Picker } from 'react-native'
 import CustomInput from '../Components/CustomInput';
 import CustomButton from '../Components/CustomButtom';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
@@ -16,6 +16,7 @@ export default class NovaPlanta extends Component {
       data: '',
       hora: '',
       altura: '',
+      medida: 'mm',
       observacao: '',
     }
 
@@ -23,7 +24,7 @@ export default class NovaPlanta extends Component {
   }
 
   _onPressButton(){
-    const {data, hora, altura, observacao} = this.state
+    const {data, hora, altura, observacao, medida} = this.state
     if (data === ""){
       Alert.alert(
         'Data inválida',
@@ -55,13 +56,19 @@ export default class NovaPlanta extends Component {
       )
       return
     }
+
+    let finalAltura = altura + medida
     
-    this.props.navigation.navigate('Planta', {novoHistorico: {data, hora, altura, observacao}})
+    this.props.navigation.navigate('Planta', {novoHistorico: {data, hora, altura: finalAltura, observacao}})
+  }
+
+  changeValue = (medida) => {
+    this.setState({medida})
   }
 
   render() {
+    const {medida} = this.state
     return (
-
       <View style={styles.container}>
         <KeyboardAwareScrollView>
           <View>
@@ -77,12 +84,27 @@ export default class NovaPlanta extends Component {
               value={this.state.hora}
               placeholder="Digite aqui a hora da coleta"
             />
-            <CustomInput
-              label={"Altura da planta"}
-              onChangeText={(altura) => this.setState({altura})}
-              value={this.state.altura}
-              placeholder="Digite aqui a altura da planta durante a coleta"
-            />
+            <View style={styles.containerMedida}>
+              <View style={styles.containerAltura}>
+                <CustomInput
+                  label={"Altura da planta"}
+                  onChangeText={(altura) => this.setState({altura})}
+                  value={this.state.altura}
+                  placeholder="Digite aqui a altura da planta durante a coleta"
+                  keyboardType='numeric'
+                />
+              </View>
+              <View style={styles.containerPicker}>
+                <Picker
+                  selectedValue={medida}
+                  style={styles.picker}
+                  onValueChange={(itemValue, itemIndex) => this.changeValue(itemValue)}>
+                    <Picker.Item label={"mm"} value={"mm"} />
+                    <Picker.Item label={"cm"} value={"cm"} />
+                    <Picker.Item label={"m"} value={"m"} />
+                </Picker>
+              </View>
+            </View>
             <CustomInput
               multiline = {true}
               numberOfLines = {3}
@@ -110,6 +132,28 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: "100%",
     bottom: 0,
+  },
+  containerMedida: {
+    width: "100%",
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  containerAltura: {
+    width: "60%",
+  },
+  containerPicker: {
+    width: "40%",
+    borderColor: '#232323', 
+    borderBottomWidth: 1,
+  },
+  picker: {
+    marginTop: 25,
+    height: 40, 
+    borderColor: '#232323', 
+    borderBottomWidth: 1,
+    backgroundColor: '#d9d9d9',
+    borderTopLeftRadius: 7,
+    borderTopRightRadius: 7,
   }
  
 });
